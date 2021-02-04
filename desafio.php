aca12e08f81fe74026ab4c8fde7ab9305fbf86be
raphaelodreis <raphaelodreis@gmail.com>
2021-02-04 08:20:29 -0300

    Adicao unit test

--- desafio.php
+++ desafio.php
@@ -1,15 +1,20 @@
 <?php
 
-for ($i=1; $i <= 100; $i++) { 
-    
-    $numeroVez = $i;
+for ($i=1; $i <= 100; $i++) 
+{
+    echo verificaNumero($i)."<br>"; 
+}
 
+
+function verificaNumero($numeroVez) 
+{
     //Calcula o resto de divisao do numero vez
     $resto3 = $numeroVez % 3;
     $resto5 = $numeroVez % 5;
-    
+
     // Condicional que verifica se o numero eh multiplo de 3 ou 5
-    if ($resto3 == 0 || $resto5 == 0) {
+    if ($resto3 == 0 || $resto5 == 0)
+    {
         $nome1 = str_replace('0', 'BHUR',$resto3);
         $nome2 = str_replace('0', 'IT',$resto5);
         
@@ -17,8 +22,8 @@ for ($i=1; $i <= 100; $i++) {
         $numeroVez = preg_replace('/[0-9]+/', '', "$nome1 $nome2");
         
         // Caso o numero seja multiplo de 3 e 5 substitui o nome
-        $numeroVez = str_replace('BHUR IT','BHUR TI',$numeroVez);
+        $numeroVez = trim(str_replace('BHUR IT','BHUR TI',$numeroVez));
     }
 
-    echo $numeroVez."<br>";
+    return $numeroVez;
 }
